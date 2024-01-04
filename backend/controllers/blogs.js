3b7db15d3ea7e35851719cf6e38de55c2ee0c5b0
Johanna Kinnari <johanna.e.kinnari@gmail.com>
2024-01-04 15:53:27 +0200

    frontend + backend yhdessä

---
+++ backend/controllers/blogs.js
@@ -0,0 +1,29 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.get('/api/blogs', (request, response) => {
+  console.log('Handling GET request to /api/notes')
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
